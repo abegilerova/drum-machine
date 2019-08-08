4034de827493d96b8d5bc1a5470e97243deeee81
abegilerova <abegilerova@gmail.com>
2019-08-07 23:43:19 -0400

    commiting App.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import DrumMachine from './components/DrumMachine';
+import Grid from '@material-ui/core/Grid';
+import {withStyles} from '@material-ui/core/styles';
+import { Container, Row, Col } from 'reactstrap';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import Button from '@material-ui/core/Button';
 
-function App() {
+
+
+
+    
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    
+      
+  
+    }
+    
+render (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid 
+    container
+        
+        alignItems="center"
+        justify="center"
+        display="flex"
+        style={{ minHeight: '100vh'}}>
+      
+       <Grid xs={11} lg={8} item>
+       
+
+    
+  <DrumMachine  />
+  </Grid>
+    </Grid>
   );
+  }
 }
 
 export default App;
